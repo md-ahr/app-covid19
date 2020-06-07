cff79a4702cf356e9b9f8a15dbf001fa92b7a394
Abdul Halim <halim@okomari.com>
2020-06-07 11:51:05 +0600

    remove build dir from gitignore

---
+++ build/precache-manifest.c5ddefcf160d0d8baa5062e67894c875.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9af2a2c189525ff575409b81958edcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7f33d55cf898f2f107",
+    "url": "/static/css/2.b450f4ca.chunk.css"
+  },
+  {
+    "revision": "036795fceedd13f0bc72",
+    "url": "/static/css/main.a64b9b51.chunk.css"
+  },
+  {
+    "revision": "dd7f33d55cf898f2f107",
+    "url": "/static/js/2.fafb7839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fafb7839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "036795fceedd13f0bc72",
+    "url": "/static/js/main.3a9ecb81.chunk.js"
+  },
+  {
+    "revision": "9f0011e7d620d13195e2",
+    "url": "/static/js/runtime-main.fc82638d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/montserrat-bold.ade91f47.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/montserrat-light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/montserrat-medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/montserrat-regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/montserrat-semibold.c641dbee.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/static/media/montserrat-thin.43dd5b7a.ttf"
+  }
+]);
